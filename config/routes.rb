ae543425e8a1a9a5c606ee3a59de2471d74a632a
Michał Olejniczak <blind.x6@gmail.com>
2018-05-20 14:27:40 +0200

    define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post 'token', to: 'authentication#token'
+      resources :users, only: [:create]
+      resources :places
+    end
+  end
 end
