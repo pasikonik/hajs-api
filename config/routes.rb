f01d4899c50815629040a2b9971980b1eb2423e5
Michał Olejniczak <blind.x6@gmail.com>
2018-05-31 15:37:18 +0200

    add all default routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'token', to: 'authentication#token'
-      resources :users, only: [:create]
+      resources :users
       resources :places
     end
   end
