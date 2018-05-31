1bbb235649452b830063e6d7f5f8ef4b8c7c1731
Michał Olejniczak <blind.x6@gmail.com>
2018-05-31 15:36:20 +0200

    add endpoint to get one place

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -2,7 +2,11 @@ module Api
   module V1
     class PlacesController < ApplicationController
       def index
-        render jsonapi: Place.all, include: params[:include]
+        render json: Place.all, include: params[:include]
+      end
+
+      def show
+        render json: Place.find(params[:id])
       end
     end
   end
