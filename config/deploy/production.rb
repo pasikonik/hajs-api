39e2dd716de496a8be42e6790ff6a3469cebab8a
Michał Olejniczak <blind.x6@gmail.com>
2018-12-21 19:28:09 +0100

    Add capistrano configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "vps628229.ovh.net", user: "deploy", roles: %w{app db}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -17,9 +17,9 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+role :app, %w{vps628229.ovh.net}
+# role :web, %w{vps628229.ovh.net}
+role :db,  %w{vps628229.ovh.net}
 
 
 
