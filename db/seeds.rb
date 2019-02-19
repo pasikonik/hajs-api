f2ca3d8ce58d814318410f446560ae1c9ce520c2
Michał Olejniczak <blind.x6@gmail.com>
2019-02-19 21:54:45 +0100

    Update database.yaml and add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+frank = User.create(email: 'frank@mail.com', password: 'password', username: 'Frank', payer: true)
+nina = User.create(email: 'nina@mail.com', password: 'password', username: 'Nina')
+kina = User.create(email: 'kina@mail.com', password: 'password', username: 'Kina')
+
+Place.create(name: 'Green House', rent: 2520, users: [frank, nina, kina])
