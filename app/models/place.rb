722e9b37f2ef89dcd7c64a9254d91d6347ba43d7
Michał Olejniczak <blind.x6@gmail.com>
2019-02-21 21:29:13 +0100

    Add bills relationship to place

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ApplicationRecord
   has_many :users
   has_many :payments
+  has_many :bills
 
   def payer
     users.find_by(payer: true)
--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -2,4 +2,5 @@ class PlaceSerializer < ActiveModel::Serializer
   attributes :name, :rent
 
   has_many :users
+  has_many :bills
 end
