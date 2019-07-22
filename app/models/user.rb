31bd9bca5a535bc213853dc86d0def8a6ec818b1
Michał Olejniczak <blind.x6@gmail.com>
2019-07-22 20:12:30 +0200

    Enable action_cable again

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_secure_password
   attr_encrypted :ban, key: Rails.application.secrets.encrypted_key
 
-  belongs_to :place, optional: false
+  belongs_to :place, optional: true
   has_many :payments, dependent: :destroy
 end
